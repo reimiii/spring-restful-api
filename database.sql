f64a0ecb3d98fd3f53ae2102eba058879facd843
Hilmi AM <imiia75775@proton.me>
2024-07-04 06:06:22 +0700

    Entity User

--- database.sql
+++ database.sql
@@ -1,3 +1,19 @@
 create database restful_api;
 
 use restful_api;
+
+create table users
+(
+    username         varchar(100) not null,
+    password         varchar(100) not null,
+    name             varchar(100) not null,
+    token            varchar(100),
+    token_expired_at bigint,
+    primary key (username),
+    unique (token)
+) engine innodb;
+
+select *
+from users;
+
+desc users;
---
+++ src/main/java/franxx/code/restful/api/entity/User.java
@@ -0,0 +1,29 @@
+package franxx.code.restful.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  private String username;
+
+  private String password;
+
+  private String name;
+
+  private String token;
+
+  @Column(name = "token_expired_at")
+  private Long tokenExpiredAt;
+}

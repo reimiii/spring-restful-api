5037479cb86d267c19d9e13d08a83c9e9eec6816
Hilmi AM <imiia75775@proton.me>
2024-07-03 14:27:32 +0700

    first commit

---
+++ src/main/java/franxx/code/restful/api/SpringRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.restful.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxx/code/restful/api/SpringRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.restful.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
